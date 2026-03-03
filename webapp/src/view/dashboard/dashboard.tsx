1f14cba2d260f1e79187e6e18b0a3fed9f1a112a
Atheeque <159514505+AtheequeFurkhan@users.noreply.github.com>
2026-03-03 14:49:42 +0530

    style: remove unnecessary blank line in dashboard.tsx

--- webapp/src/view/dashboard/dashboard.tsx
+++ webapp/src/view/dashboard/dashboard.tsx
@@ -14,7 +14,6 @@
 // specific language governing permissions and limitations
 // under the License.
 
-
 import TabsPage from "@layout/pages/TabsPage";
 import UnderDevelopment from "@src/layout/pages/Maintenance";
 
