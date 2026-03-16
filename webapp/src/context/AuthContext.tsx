1648e27e0678d661bbd1624c652d970173fd34a6
Atheeque <159514505+AtheequeFurkhan@users.noreply.github.com>
2026-03-16 16:24:30 +0530

    feat: rename opdClaimSummary resource to opd-claims for consistency

--- webapp/src/context/AuthContext.tsx
+++ webapp/src/context/AuthContext.tsx
@@ -13,7 +13,6 @@
 // KIND, either express or implied.  See the License for the
 // specific language governing permissions and limitations
 // under the License.
-
 import { SecureApp, useAuthContext } from "@asgardeo/auth-react";
 import { useIdleTimer } from "react-idle-timer";
 
@@ -24,6 +23,7 @@ import SessionWarningDialog from "@component/common/SessionWarningDialog";
 import LoginScreen from "@component/ui/LoginScreen";
 import { redirectUrl } from "@config/constant";
 import { loadPrivileges, setAuthError, setUserAuthData } from "@slices/authSlice/auth";
+import { fetchAppConfig } from "@slices/configSlice/config";
 import { useAppDispatch } from "@slices/store";
 import { getUserInfo } from "@slices/userSlice/user";
 import { APIService } from "@utils/apiService";
@@ -123,6 +123,7 @@ const AppAuthProvider = (props: { children: React.ReactNode }) => {
 
     await dispatch(getUserInfo());
     await dispatch(loadPrivileges());
+    await dispatch(fetchAppConfig());
   }, [getBasicUserInfo, getIDToken, getDecodedIDToken, dispatch, refreshToken]);
 
   const appSignIn = useCallback(async () => {
